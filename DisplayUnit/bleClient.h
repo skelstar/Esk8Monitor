2a8f6e0e9081a6af746c94395ec82c1fd3b570fd
skelstar <skelstar@gmail.com>
2019-04-06 06:42:31 +1300

    - ported code over to DisplayUnit from BLEClient - made some .h files - reconnects when disconnected - doesn't display anything

--- DisplayUnit/bleClient.h
+++ DisplayUnit/bleClient.h
@@ -1,22 +1,3 @@
-#include <Arduino.h>
-#include <U8g2lib.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <myPushButton.h>
-#include "BLEDevice.h"
-#include <driver/adc.h>
-
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
-struct STICK_DATA
-{
-  float batteryVoltage;
-  float motorCurrent;
-  bool moving;
-  bool vescOnline;
-};
-STICK_DATA stickdata;
 
 /* ---------------------------------------------- */
 static BLEAddress *pServerAddress;
@@ -45,39 +26,8 @@ static void notifyCallback(
   size_t length,
   bool isNotify)
 {
-
   memcpy(&stickdata, pData, sizeof(stickdata));
   Serial.printf("Received batteryVoltage: %.1f \n", stickdata.batteryVoltage);
-}
-
-void setup()
-{
-
-  Serial.begin(9600);
-  Serial.println("\nStarting Arduino BLE Client application...");
-
-  bleConnectToServer();
-}
-
-long now = 0;
-
-void loop()
-{
-  if (connected && millis() - now > 2000)
-  {
-    now = millis();
-    sendToMaster();
-  }
-  delay(200);
-}
-
-void sendToMaster()
-{
-  Serial.printf("sending to master\n");
-  // char buff[6];
-  // ltoa(millis(), buff, 10);
-  // pRemoteCharacteristic->writeValue(buff, sizeof(buff));
-  pRemoteCharacteristic->writeValue("test", sizeof("test"));
 }
 
 bool bleConnectToServer()
